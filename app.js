d74ea222375782dcf8d54c9340f3edf9d002bb59
jungahhwang <snowgirl0113@naver.com>
2021-06-09 18:30:24 +0900

    1

---
+++ app.js
@@ -0,0 +1,26 @@
+var express = require('express');
+const { allowedNodeEnvironmentFlags } = require('process');
+var app = express()
+
+var contents = [
+    {
+    id : 0,
+    name : '안서후'
+    },
+    {
+    id : 1,
+    name : '안서준'
+    },
+    {
+    id : 2,
+    name : '황정아1'
+    }
+]
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/',  function(req, res)  {
+  //res.send('hello world')
+  res.json(contents);
+})
+
+app.listen(5000, () => console.log('listening on port 5000'));
